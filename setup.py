b771a4b0000bf257541a61f430e30a1aeb9038e2
slidemoon <moon93110304@gmail.com>
2019-06-26 15:39:14 +0800

    define version for lxml package

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     ],
     install_requires=[
         'XBlock',
-        'lxml',
+        'lxml==3.8.0',
     ],
     dependency_links=[
         'git+https://github.com/Learningtribes/xblock-utils.git@ec95e5e718c4144dc8a43d116a545f210d929667#egg=xblock-utils',
