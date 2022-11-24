e3eda5676255f753a001df3865fbd0b5a817e2d5
tassadit <tassadite.sadoun@gmail>
2022-11-24 16:50:34 +0100

    add new version fs

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'lxml==3.8.0',
         'web-fragments==0.2.2',
         'user-agents==2.1',
-        'fs==2.4.13',
+        'fs==2.0.18',
     ],
     dependency_links=[
         'git+https://github.com/Learningtribes/xblock-utils.git@ec95e5e718c4144dc8a43d116a545f210d929667#egg=xblock-utils',
