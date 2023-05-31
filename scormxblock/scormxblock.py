df5dcc20cb769dc3651f63da989ccbd0e8f56267
tassadit <tassadite.sadoun@gmail>
2023-05-31 14:06:59 +0200

    add log

--- scormxblock/scormxblock.py
+++ scormxblock/scormxblock.py
@@ -440,6 +440,7 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
                     logger.info("only_value: " +str(data[k]))
                     logger.info("data data[k]: " +str(data))
                 data["{}_value".format(k)] = getattr(self, k)
+                logger.info("value.format(k) : " +str(["{}_value".format(k)]))
                 logger.info("value.format(k) : " +str(data["{}_value".format(k)]))
                 logger.info(" getattr(self, k) : " +str( getattr(self, k)))
                 logger.info("format(k) : " +str(data))
