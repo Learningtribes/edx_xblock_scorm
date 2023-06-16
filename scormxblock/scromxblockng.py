a3d8e73ed075e9dc7b0aa5ad6b5034560bd27610
lpfaender <laetitia.pfaender@gmail.com>
2023-06-16 20:47:55 +0800

    SCORM 1.2: max default as 1 or 100 depending on raw

--- scormxblock/scromxblockng.py
+++ scormxblock/scromxblockng.py
@@ -557,9 +557,12 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
     def extract_runtime_info_12(data):
         info = {'status': SCORM_STATUS.IN_PROGRESS}
         if 'cmi.core.score.raw' in data:
-            info["raw"] = float(data['cmi.core.score.raw'])
-            info["maxi"] = float(data.get('cmi.core.score.max', 1.0))
-            info["mini"] = float(data.get('cmi.core.score.min', 0.0))
+            info['raw'] = float(data['cmi.core.score.raw'])
+            info['mini'] = float(data.get('cmi.core.score.min', 0.0))
+            if 'cmi.core.score.max' in data:
+                info['maxi'] = float(data.get('cmi.core.score.max'))
+            else:
+                info['maxi'] = 100.0 if info['raw'] > 1 else 1.0
 
         lesson_status = data.get('cmi.core.lesson_status', SCORM_STATUS.IN_PROGRESS)
 
@@ -574,9 +577,9 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
     def extract_runtime_info_2004(data):
         info = {'status': SCORM_STATUS.IN_PROGRESS}
         if 'cmi.score.raw' in data and 'cmi.score.max' in data and 'cmi.score.min' in data:
-            info["raw"] = float(data['cmi.score.raw'])
-            info["maxi"] = float(data['cmi.score.max'])
-            info["mini"] = float(data['cmi.score.min'])
+            info['raw'] = float(data['cmi.score.raw'])
+            info['maxi'] = float(data['cmi.score.max'])
+            info['mini'] = float(data['cmi.score.min'])
         elif 'cmi.score.scaled' in data:
             info['raw'] = float(data['cmi.score.scaled'])
             info['maxi'] = 1.0
@@ -600,8 +603,8 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
 
         score = None
         if 'raw' in info:
-            score = Score(raw_earned=(info["raw"] - info["mini"]),
-                          raw_possible=(info["maxi"] - info["mini"]))
+            score = Score(raw_earned=(info['raw'] - info['mini']),
+                          raw_possible=(info['maxi'] - info['mini']))
 
         if score and (not self.has_submitted_answer() or self.allows_rescore()):
             self.set_score(score)
