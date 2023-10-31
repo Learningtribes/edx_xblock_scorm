2713c5e0e41e93ceddf7b8d09f77a84e3e12267e
noahwang <234082230@qq.com>
2023-10-31 17:54:17 +0800

    add scorm zip upload

--- scormxblock/scormxblock.py
+++ scormxblock/scormxblock.py
@@ -342,10 +342,6 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
             bytes_io = BytesIO(binary_data)
             bytes_io.seek(0)
             file_path = pkg_id.decode('utf-8') + '.zip'
-            # memory_fs = MemoryFS()
-            # with memory_fs.open(file_path, 'wb') as fp:
-            #     fp.write(bytes_io.read())
-            # copy_file(memory_fs, file_path, self.fs, file_path)
             with self.fs.open(file_path, 'wb', acl='public-read') as s3_file:
                 s3_file.write(bytes_io.read())
         except IOError:
