29cc20a190ba951ef7168bab2635209681be71d7
noahwang <234082230@qq.com>
2023-11-14 15:04:29 +0800

    display scorm_pkg_filename

--- scormxblock/scormxblock.py
+++ scormxblock/scormxblock.py
@@ -162,6 +162,7 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
         display_name=_("SCORM filename"),
         scope=Scope.settings,
         default="",
+        allow_reset=False,
         readonly=True
     )
 
