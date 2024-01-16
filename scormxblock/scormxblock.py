470cc76cfaa4f2df4eed75b56ef13bbfd14c16c3
Barry Bao <barrybao@Barrys-Mac-mini.local>
2024-01-16 15:09:31 +0800

    fixed

--- scormxblock/scormxblock.py
+++ scormxblock/scormxblock.py
@@ -318,9 +318,10 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
                     logger.info('[WARN] uuid not found in path {}.'.format(self.scorm_pkg))
 
             else:
-                S3_BUCKET_NAME = settings.DJFS.get('bucket')
                 _s3_prefix = self.fs.dir_path[1:]      # Sample: /xblock/block--v1-_beta-.Content__demo-.2020Q1-.type_64_scormxblock-.block_64_ae63e8b39db84405a8763c9a5441f93c/fs/NONE.NONE
-                _pkg_uuid = self.scorm_pkg.split('/')[0]
+                logger.info('[INFO] Removing AWS S3 Old SCORM Packages by PREFIX={}...'.format(_s3_prefix))
+
+                S3_BUCKET_NAME = settings.DJFS.get('bucket')
                 _delete_keys = {'Objects': []}
                 objects_to_delete = self.fs.client.list_objects_v2(Bucket=S3_BUCKET_NAME, Prefix=_s3_prefix)
                 _delete_keys['Objects'] = [
