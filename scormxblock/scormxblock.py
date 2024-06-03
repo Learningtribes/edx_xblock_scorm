59672ac0433b444326230eac8044060802ac2225
Barry Bao <barrybao@Barrys-Mac-mini.local>
2024-06-03 18:08:34 +0800

    fixed

--- scormxblock/scormxblock.py
+++ scormxblock/scormxblock.py
@@ -751,6 +751,8 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
         lesson_status = data.get('cmi.completion_status')
         if lesson_status == 'completed':
             info['status'] = SCORM_STATUS.SUCCEED
+        elif lesson_status == 'incomplete':
+            info['status'] = SCORM_STATUS.IN_PROGRESS
 
         return info
 
@@ -777,6 +779,8 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
         success_status = data.get('cmi.completion_status')
         if success_status == 'completed':
             info['status'] = SCORM_STATUS.SUCCEED
+        elif success_status == 'incomplete':
+            info['status'] = SCORM_STATUS.IN_PROGRESS
 
         return info
 
