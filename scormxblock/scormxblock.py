84531034e797f3757819f4e31bd7a5919755bc42
Barry Bao <barrybao@Barrys-Mac-mini.local>
2025-06-13 15:19:41 +0800

    added

--- scormxblock/scormxblock.py
+++ scormxblock/scormxblock.py
@@ -895,7 +895,7 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
         info = {'status': SCORM_STATUS.IN_PROGRESS}
         if 'cmi.core.score.raw' in data:
             score_raw = data.get('cmi.core.score.raw')
-            info['raw'] = float(data.get('cmi.core.score.raw')) if score_raw is not None else 0.0
+            info['raw'] = 0.0 if score_raw is None else float(data.get('cmi.core.score.raw'))
             info['mini'] = float(data.get('cmi.core.score.min', 0.0))
             if 'cmi.core.score.max' in data:
                 info['maxi'] = float(data.get('cmi.core.score.max'))
