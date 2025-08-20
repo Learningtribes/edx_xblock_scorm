88dd2dfb7cc7c25ba02e6a8983659df1d342d42e
Barry <26813045+BarryPaneer@users.noreply.github.com>
2025-08-20 17:53:44 +0800

    always allow rescore (#59)
    * scorm_allow_rescore set to true by default
    * rescore option removed from UI
    * we publish a score only if the SCORM is supposed to be graded (has_score)

--- scormxblock/scormxblock.py
+++ scormxblock/scormxblock.py
@@ -232,7 +232,7 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
     )
 
     scorm_allow_rescore = Boolean(
-        default=False,
+        default=True,
         scope=Scope.settings,
         enforce_type=True,
         display_name=_("Rescore"),
@@ -276,7 +276,7 @@ class ScormXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
 
     editable_fields = (
         'scorm_pkg', 'scorm_pkg_filename', 'display_name',
-        'has_score', 'weight', 'scorm_allow_rescore',
+        'has_score', 'weight',
         'open_new_tab', 'instruction', 'cover_image'
     )
     has_author_view = True
