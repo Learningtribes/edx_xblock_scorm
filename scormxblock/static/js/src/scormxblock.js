abcf888578d7417e2d16b387ae4e6e22bc9ac094
Barry Bao <barrybao@Barrys-Mac-mini.local>
2025-09-30 17:33:10 +0800

    added

--- scormxblock/static/js/src/scormxblock.js
+++ scormxblock/static/js/src/scormxblock.js
@@ -35,7 +35,7 @@
                 let usageId = element.dataset.usageId;
                 $('.xblock-student_view[data-usage-id="'+usageId+'"]').addClass('fullscreen_scormxblock_view');
                 $('.xblock-student_view[data-usage-id="'+usageId+'"] #scorm-object-frame').removeClass('hidden');
-                $('.xblock-student_view[data-usage-id="'+usageId+'"] #id-scormxblock-launcher').addClass('hidden');
+                $('.xblock-student_view[data-usage-id="'+usageId+'"] .launch-div').addClass('hidden');
 
                 $('.launch-button').addClass('disabled');
                 $('.launch-before').toggleClass('hidden');
