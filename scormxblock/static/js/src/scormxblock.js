830a508c11ef74736a7ce72886034e1c0858ed3d
Barry Bao <barrybao@Barrys-Mac-mini.local>
2025-10-13 11:41:37 +0800

    fixed

--- scormxblock/static/js/src/scormxblock.js
+++ scormxblock/static/js/src/scormxblock.js
@@ -490,6 +490,13 @@
                 window.API_1484_11 = new SCORM_2004_API();
             })
 
+            $(document).on('keydown', function(e) {
+                 if (e.keyCode === 27 || e.key === 'Escape') {
+                     console.log('ESC pressed!');
+                     quitFullscreen();
+                 }
+            });
+
             if (!window.loadingScormModuleMap) {
                 window.loadingScormModuleMap = {}
             }
@@ -520,13 +527,6 @@
                     }
                 }
             })
-
-            $(document).on('keydown', function(e) {
-                 if (e.key === 'Escape') {
-                     console.log('ESC pressed！');
-                     quitFullscreen();
-                 }
-            });
 
         });
     }
