1977aac6c3450bc4a240690b88b22c274fa4843f
Barry Bao <barrybao@Barrys-Mac-mini.local>
2025-10-13 16:49:40 +0800

    added back: refreshing function

--- scormxblock/static/js/src/scormxblock.js
+++ scormxblock/static/js/src/scormxblock.js
@@ -28,6 +28,22 @@
             iframe_container.removeClass('fullscreen_scormxblock_view');
 
             iframe.css('height', '0px');
+
+            function updateQueryParam(url, key, value) {
+                var separator = url.indexOf('?') !== -1 ? '&' : '?';
+                var re = new RegExp('([?&])' + key + '=[^&]*');
+                if (re.test(url)) {
+                    return url.replace(re, '$1' + key + '=' + encodeURIComponent(value));
+                } else {
+                    return url + separator + key + '=' + encodeURIComponent(value);
+                }
+            }
+            // For some SCORM pkgs, they show "Ending Text" after learner clicks on "Exit Button".
+            // So we refresh the iframe.src to reload the content before the learner clicks the "Launch Button" again.
+            let newURL = updateQueryParam(iframe[0].src, 'lt_refresh_time', new Date().getTime());
+            iframe[0].src = newURL;
+            iframe.attr('data-src', newURL);
+
         }
 
         function scormInit() {
@@ -59,10 +75,7 @@
                     iframe_container.addClass('fullscreen_scormxblock_view');
                     $(container + '[data-usage-id="'+usageId+'"] .scorm-object-header').removeClass('hidden');
 
-                    iframe.on('load', function() {
-                    $(this).css('height', '100vh');
-                        iframe.css('height', '100vh');
-                    });
+                    iframe.css('height', '100vh');
                 }
             })
 
